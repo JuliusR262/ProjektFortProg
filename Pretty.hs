0f948da0a714fa7ac698f7e9ce379ee5c12a5cdf
Julius Rüder <juliusrueder@gmail.com>
2020-02-18 14:25:59 +0100

    init 2

---
+++ Pretty.hs
@@ -0,0 +1,19 @@
+import Type
+import Data.List
+
+class Pretty a where
+    pretty :: a -> String
+
+instance Pretty (Term) where
+   
+    pretty (Var vName)          = vName
+    pretty (Comb cName [])      = cName
+    pretty (Comb "." [t1,t2])   = case t2 of 
+                                    Comb "[]" []  -> "[" ++ pretty t1 ++ "]"
+                                    Comb "." ts   -> "[" ++ pretty t1 ++ "," 
+                                                         ++ init (tail (pretty (Comb "." ts))) 
+                                                         ++ "]"
+                                    _             -> "[" ++ pretty t1 ++ "|" ++  pretty t2 ++ "]"
+    pretty (Comb cName t2)      = cName ++ "(" ++ intercalate ", " (map pretty (t2)) ++ ")"
+   
+     
---
+++ Vars.hs
@@ -0,0 +1,31 @@
+import Type
+
+class Vars a where
+    allVars :: a -> [VarName]
+
+freshVars :: [VarName]
+freshVars =  (freshVars' 0)
+
+freshVars' :: Int -> [VarName]
+freshVars' x = ('_' : (show x)) : (freshVars' (x+1))
+        
+instance Vars (Term) where
+    
+    allVars (Var vName)         = [vName]
+    allVars (Comb _ [])         = []
+    allVars (Comb _ (x:xs))     = (allVars x) ++ (allVars (Comb "" xs))
+    
+    
+instance Vars (Rule) where
+
+    allVars (Rule x xs)         = (allVars x) ++ (allVars (Comb "" xs))
+    
+instance Vars (Prog) where
+    
+    allVars (Prog [])           = []
+    allVars (Prog (x:xs))       = (allVars x) ++ (allVars (Prog xs))
+    
+instance Vars (Goal) where
+
+    allVars (Goal xs)           = allVars (Comb "" xs)
+    
