29b93dc71c4fcbdfdbcc81c4ac71d71e980caef1
Sebastian Falkowski <stu216880@informatik.uni-kiel.de>
2020-02-18 16:33:34 +0100

    Substitutionen und so v2

---
+++ Subst Old.hs
@@ -0,0 +1,31 @@
+import Type
+
+import Pretty
+
+-- Data type for substitutions
+data Subst = Subst [(VarName, Term)]
+  deriving Show
+
+-- The empty substitution
+empty :: Subst
+empty = Subst []
+
+-- One single substitution
+single :: VarName -> Term -> Subst
+single vName term = Subst [(vName, term)]
+
+
+-- Applys a substitution to a term
+apply :: Subst -> Term -> Term
+apply (Subst []) term     = term
+apply (Subst (x:xs)) term = apply (Subst xs) (applySingle x term)
+    where applySingle :: (VarName, Term) -> Term -> Term
+          applySingle (svName, sTerm) (Var destvName)
+              | svName == destvName = sTerm
+              | otherwise = (Var destvName)
+          applySingle (svName, sTerm) (Comb destcName destTerm) = (Comb destcName (map (applySingle (svName, sTerm)) destTerm))
+              
+
+-- Geht vllt noch besser
+compose :: Subst -> Subst -> Subst
+compose (Subst xs) (Subst ys) = Subst (xs ++ ys)
--- Substitution.hs
+++ Substitution.hs
@@ -1,6 +1,5 @@
 import Type
-
-import Pretty
+--import Pretty
 
 -- Data type for substitutions
 data Subst = Subst [(VarName, Term)]
@@ -15,13 +14,33 @@ single :: VarName -> Term -> Subst
 single vName term = Subst [(vName, term)]
 
 
+
+
 -- Applys a substitution to a term
 apply :: Subst -> Term -> Term
-apply (Subst []) term     = term
-apply (Subst (x:xs)) term = apply (Subst xs) (applySingle x term)
-    where applySingle :: (VarName, Term) -> Term -> Term
-          applySingle (svName, sTerm) (Var destvName)
-              | svName == destvName = sTerm
-              | otherwise = (Var destvName)
-          applySingle (svName, sTerm) (Comb destcName destTerm) = (Comb destcName (map (applySingle (svName, sTerm)) destTerm))
-              
+apply (Subst []) term = term
+apply (Subst ((svName, sTerm):xs)) (Var vName)
+    | svName == vName = sTerm
+    | otherwise = (apply (Subst xs) (Var vName))
+apply subst (Comb cName cTerm) = (Comb cName (map (apply subst) cTerm))
+--apply (Subst []) term     = term
+--apply (Subst (x:xs)) term = apply (Subst xs) (applySingle x term)
+--    where applySingle :: (VarName, Term) -> Term -> Term
+--          applySingle (svName, sTerm) (Var destvName)
+--              | svName == destvName = sTerm
+--              | otherwise = (Var destvName)
+--          applySingle (svName, sTerm) (Comb destcName destTerm) = (Comb destcName (map (applySingle (svName, sTerm)) destTerm))
+
+          
+--compose :: Subst -> Subst -> Subst
+--compose (Subst xs) (Subst ys) 
+
+--case x of
+                                   --  (vName,sTerm) -> sTerm
+                                   --  _ -> (apply (Subst xs) (Var vName))
+
+subst1 = (Subst [("A", Comb "f" [Var "B", Var "_", Comb "true" []]),("B", Comb "false" [])])
+subst2 = (Subst [("B", Comb "false" [])])
+term1 =  (Comb "." [Var "K",Comb "." [Var "L",Var "A",Var "B", Var "B"]])
+
+--pretty1 = pretty (apply subst1 term1)
