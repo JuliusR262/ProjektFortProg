132fad0d51f976688f9dcb2b99f38b04cc13e85f
Sebastian Falkowski <stu216880@informatik.uni-kiel.de>
2020-02-19 14:14:10 +0100

    Die neue ultimative Substitution

--- Substitution.hs
+++ Substitution.hs
@@ -32,7 +32,6 @@ substConcat :: Subst -> Subst -> Subst
 substConcat (Subst xs) (Subst ys) = Subst (xs ++ ys)
 
 
-
 -- Applys a substitution to a term
 apply :: Subst -> Term -> Term
 apply (Subst []) term = term
@@ -49,6 +48,12 @@ apply subst (Comb cName cTerm) = (Comb cName (map (apply subst) cTerm))
 --          applySingle (svName, sTerm) (Comb destcName destTerm) = (Comb destcName (map (applySingle (svName, sTerm)) destTerm))
 
 
+findSubst :: VarName -> Subst -> Maybe (VarName, Term)
+findSubst _ (Subst []) = Nothing
+findSubst vName (Subst ((s3vName, s3Term):s3))  
+                                | vName == s3vName = Just (s3vName, s3Term)
+                                | otherwise = findSubst vName (Subst s3)
+
           
 compose :: Subst -> Subst -> Subst
 compose (Subst []) (Subst s1) = (Subst s1)
@@ -56,22 +61,22 @@ compose (Subst s2) (Subst []) = (Subst s2)
 compose (Subst (x2:s2)) (Subst (x1:s1)) = let 
                                           (s2vName, s2Term) = x2
                                           (s1vName, s1Term) = x1
-                                          findSubst :: VarName -> Subst -> Maybe (VarName, Term)
-                                          findSubst _ (Subst []) = Nothing
-                                          findSubst vName (Subst ((s3vName, s3Term):s3))
-                                              | vName == s3vName = Just (s3vName, s3Term)
-                                              | otherwise = findSubst vName (Subst s3)
                                           applyToAll :: Subst -> [(VarName,Term)] -> [(VarName,Term)]
                                           applyToAll substToApply [] = []
-                                          applyToAll substToApply ((s3vName,s3Term):s3) = (s3vName, (apply substToApply s3Term)) : (applyToAll substToApply s3)
-                                          
+                                          applyToAll substToApply ((s3vName,s3Term):s3) = (s3vName, (apply substToApply s3Term)) : (applyToAll substToApply s3)      
                                           -- If s2 has a rule that s1 doesnt , apply it to everything in s1 and put it on top. Look for more rules that only s2 has in the next iteration.
                                           in if (findSubst s2vName (Subst (x1:s1))) == Nothing then compose (Subst s2) (Subst (x2:(applyToAll (Subst [x2]) (x1:s1))))
                                                                                   else compose (Subst s2) (Subst (x1:s1))
     
           
+restrictTo :: [VarName] -> Subst -> Subst
+restrictTo [] (Subst _) = Subst []
+restrictTo (v1:vs) (Subst s) = let x = (findSubst v1 (Subst s))
+                        in  case x of
+                                Just y  -> substConcat (Subst [y]) (restrictTo vs (Subst s))
+                                _       -> restrictTo vs (Subst s)
 
-
+          
 --case x of
                                    --  (vName,sTerm) -> sTerm
                                    --  _ -> (apply (Subst xs) (Var vName))
