04001562a53e19b796393d13da6a10fc867bd4ba
Julius Rüder <juliusrueder@gmail.com>
2020-02-21 09:42:23 +0100

    Vars.hs comments deutsch->english

--- Vars.hs
+++ Vars.hs
@@ -5,33 +5,33 @@ import Type
 class Vars a where
     allVars :: a -> [VarName]
 
--- unendliche Liste Prolog gültiger Variablennamen
---    mit Hilfsfunktion freshVars'
+-- infinit list of prolog valid names of variables
+--    with helper function
 freshVars :: [VarName]
 freshVars =  (freshVars' 0)
 
--- beginnt bei ["_0","_1","_2" ...]
+-- begins with ["_0","_1","_2" ...]
 freshVars' :: Int -> [VarName]
 freshVars' x = ('_' : (show x)) : (freshVars' (x+1))
 
--- Instanzen für Term, Rule, Prog, Goal
---  zum implementieren von allVars
---  welche von allen enthaltenen Variablen den Namen wiedergibt     
+-- instances for Term, Rule, Prog, Goal
+--   to implement allVars
+--  which all returns a list of names of the variables inside     
 instance Vars (Term) where
-  -- konkateniert rekursiv alle Variablennamen in einer Liste aneinander
+  -- concat recursivly all names of variables to one list together
   allVars (Var vName)         = [vName]
   allVars (Comb _ [])         = []
   allVars (Comb _ (x:xs))     = (allVars x) ++ (allVars (Comb "" xs))
   
 instance Vars (Rule) where
-  -- baut auf allVars von Term auf aufgrund Rule Term [Term]
+  -- build on allVars of Term because Rule Term [Term]
   allVars (Rule x xs)         = (allVars x) ++ (allVars (Comb "" xs))
   
 instance Vars (Prog) where
-  -- baut auf allVars von Rule auf aufgrund Prog [Rule]
+  -- build on allVars of Rule because Prog [Rule]
   allVars (Prog [])           = []
   allVars (Prog (x:xs))       = (allVars x) ++ (allVars (Prog xs))
   
 instance Vars (Goal) where
-  -- baut auf allVars Term auf aufgrund Goal [Term]
+  -- build on allVars of Term because Goal [Term]
   allVars (Goal xs)           = allVars (Comb "" xs)
