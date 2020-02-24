1d74609a45a2f9084541c3314487e6bdf249f2b4
ab321 <juliusrueder@gmail.com>
2020-02-24 16:57:38 +0100

    half of rename

--- Umbennung.hs
+++ Umbennung.hs
@@ -1,14 +1,22 @@
+module Umbennung (rename) where
 import Substitution
 import Type
 import Vars
 import Pretty
-import Control.Monad.State
+--import Control.Monad.State
  
  
 type MyState = [(VarName, VarName)]
 
-getUnusedV :: MyState -> VarName
-getUnusedV s = head( [fvvName | fvvName <- freshVars, not(elem fvvName (snd(unzip s)))] )
+type Forbidden = [VarName]
+
+getUnusedV :: Forbidden -> VarName
+getUnusedV s = head( [fvvName | fvvName <- freshVars
+                              , not(elem fvvName s)] )
+
+getUnusedVF :: MyState -> Forbidden -> VarName
+getUnusedVF s fbs = head( [fvvName | fvvName <- freshVars
+                              , not(elem fvvName (snd(unzip s))), not (elem fvvName fbs)] )                              
  
 expandState :: MyState -> VarName -> VarName -> MyState
 expandState state newVar assignedVar = (newVar, assignedVar) : state
@@ -16,8 +24,104 @@ expandState state newVar assignedVar = (newVar, assignedVar) : state
 getVar :: VarName -> MyState -> Maybe VarName
 getVar v s  = lookup v s 
 
-type MyStateMonad = State MyState
+emptyMS :: MyState
+emptyMS = []
 
+mStoSubst :: MyState -> Subst
+mStoSubst ms =let (vs,fvs) = unzip ms in 
+              Subst (zip vs (map (\x -> Var x) fvs))
+
+rename :: Rule -> Forbidden -> Rule
+rename (Rule r rs) vs = let   (Rule x xs) = (Rule r rs)
+                              substi = mStoSubst (buildSubst (allVars(Rule x xs)) vs emptyMS) in
+                        Rule (apply substi x) (map (apply substi) xs)
+
+buildSubst :: [VarName] -> Forbidden -> MyState -> MyState
+buildSubst [] _ st        = st
+buildSubst (v:vs) fbs st  = if((getVar v st) == Nothing) then
+                              buildSubst vs fbs (expandState st v (getUnusedVF st fbs))
+                            else buildSubst vs fbs st
+
+
+
+
+
+
+renameWild :: [Term] -> Forbidden -> ([Term],Forbidden)
+renameWild []      vs = ([], vs)
+renameWild (t1:ts) vs = let (t2,f) = (renameWild' t1 vs) in
+                        ((t2 : (fst(renameWild ts (f ++ vs))),(f ++ vs))
+ where  renameWild' :: Term -> Forbidden -> (Term,Forbidden) 
+        renameWild' tt vss = case tt of
+                            (Var "_")       ->  let x = (getUnusedV vss) in
+                                                ((Var x),[x])
+                            (Comb cname xs) ->  let (ttt,ff) = (renameWild xs vss) in
+                                                ((Comb cname ttt),(ff++vss))
+                            _               -> (tt,vss)
+                            
+
+
+
+
+
+
+
+
+
+
+-- rename (Rule (Var "X") []) []
+-- rename (Rule (Var "X") [(Var "Y")]) [("_0")]
+
+
+
+
+
+
+
+
+
+{-
+getUnusedV :: MyState -> VarName
+getUnusedV s = head( [fvvName | fvvName <- freshVars
+                              , not(elem fvvName (snd(unzip s)))] )
+
+-}
+
+
+{-
+rename :: Rule -> Forbidden -> Rule
+rename (Rule r rs) fbs = rename' (allVars (Rule r rs)) fbs emptyMS (Rule r rs) 
+ where rename' :: [VarName] -> Forbidden -> MyState -> Rule -> Rule       
+       rename' [] _ _ (Rule r rs) = (Rule r rs)
+       rename' (v1:vs) fvs st (Rule r rs)   
+        | (elem (getUnusedV st) fvs) = let v2 = getUnusedV st in 
+                      rename' (v1:vs) fvs (expandState v2 v2) (Rule r rs)
+        | otherwise = case getVar v1 of
+                            Nothing -> let v2 = getUnusedV st in 
+                                          rename vs  fvs (expandState st v1 v2) 
+                                          (Rule (apply (single v1 (Var (v2))) r) (map (apply (single v1 (Var v2))) rs))
+                            _       -> let Just x = (getVar v1 st) in 
+                                          rename' vs  fvs st (Rule (apply (single v1 (Var x)) r) 
+                                          (map (apply (single v1 (Var x))) rs))
+-}                                                        
+
+
+{-
+rename :: Goal -> Rule -> Rule
+rename g (Rule t ts) = rename' (allVars g) (freshVars) (t:ts) 
+ where
+    rename' :: [VarName] -> [VarName] -> [Term] -> Rule
+    rename'  []     _  (t:ts)  = Rule t ts
+    rename' (v:vs) (u:us) ts   = rename' vs us (map(apply(single v (Var u)))ts)
+    
+-}
+
+
+
+
+{-
+
+type MyStateMonad = State MyState
 
 showState :: MyStateMonad -> String
 showState (State [])   = show []
@@ -47,7 +151,7 @@ main = do
   print(getVar [("A","_0"),("B","_1"),("C","_2"),("E","_3")] "Q")
 
 
-{-
+
 valFromState :: MyState -> VarName
 valFromState v = let usedVars = snd (unzip v)
                   in vhelp' usedVars freshVars
