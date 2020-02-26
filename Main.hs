a1ef12efa52f09f7377351e70a06755ee9fa1359
ab321 <juliusrueder@gmail.com>
2020-02-26 21:52:24 +0100

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Type
 import Parser
 import Data.Either
 import Data.List()
-import Pretty
+import Pretty()
 import Substitution
 
 header,string,stratS1,stratS2,stratSF,help :: String
@@ -48,16 +48,21 @@ process x rst = do case filter (/=' ') x of
                                           if(isRight y) then 
                                             putStrLn "Loaded."
                                             else putStrLn ("Failed Loading.")
-                                          query (REPLState (fromRight p y) st)
+                                          query (REPLState (fromRight' p y) st)
                     (':':'s':strat) -> setStrat strat rst
                     _               -> do let y =(parse x) :: Either String Goal
                                           if(isRight y) then do
                                             let (REPLState prog st) = rst
                                             let substs = solve st prog (
-                                                    fromRight (Goal []) y)
+                                                    fromRight' (Goal []) y)
                                             checkEmpty substs
                                             else putStrLn "Failed loading Goal!"
                                           query rst
+
+fromRight' :: b -> Either a b -> b
+fromRight' _ (Right b)  = b
+fromRight' b  _         = b
+
 checkEmpty,output :: [Subst] -> IO()
 checkEmpty substs = case substs of
                          [] ->  putStrLn "false"
@@ -83,22 +88,3 @@ setStrat strat (REPLState prog st) = case strat of
                                   query (REPLState prog dfs)
                       _     -> do putStrLn (stratSF)
                                   query (REPLState prog st)
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
